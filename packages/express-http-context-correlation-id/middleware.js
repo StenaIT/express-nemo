61ea6cc86e6591ca72f9ae7f062e1ea362f2797b
Kristoffer Ahl <git@77dynamite.com>
2018-05-23 14:09:34 +0200

    - Implemented middleware correlation-id.

--- packages/express-http-context-correlation-id/middleware.js
+++ packages/express-http-context-correlation-id/middleware.js
@@ -1,3 +1,10 @@
+const querystring = require('querystring')
+const url = require('url')
+const uuid = require('uuid/v4')
+
 module.exports = (req, res, next) => {
+  const query = querystring.parse(url.parse(req.url).query)
+  const correlationId = query.correlationId || uuid()
+  req.context = { ...req.context, correlationId: correlationId }
   next()
 }
--- packages/express-http-context-correlation-id/middleware.spec.js
+++ packages/express-http-context-correlation-id/middleware.spec.js
@@ -1,8 +1,68 @@
 const expect = require('chai').expect
-const SUT = require('./middleware')
+const middleware = require('./middleware')
+
+const inner = (req, res) => {
+  res.body = {}
+  res.getHeader = name => {
+    return 'application/json'
+  }
+}
 
 describe('express-http-context-correlation-id', () => {
-  it('has a test', () => {
-    expect(true).to.equal(true)
+  let nextCalled = false
+
+  const next = () => {
+    nextCalled = true
+  }
+
+  beforeEach(() => {
+    nextCalled = false
+  })
+
+  it('should always call next', () => {
+    let req = { url: '/api/path' }
+    let res = {}
+
+    middleware(req, res, next)
+
+    expect(nextCalled).to.be.true
+  })
+
+  context('when request does not contain a context property', () => {
+    it('should not fail', () => {
+      let req = { url: '/api/path' }
+      let res = {}
+
+      middleware(req, res, next)
+
+      expect(req.context).to.not.be.an('undefined')
+      expect(req.context).to.not.be.null
+    })
+  })
+
+  context("when client don't supply a correlation id in request", () => {
+    it('should generate a new correlation id', () => {
+      let req = { url: '/api/path' }
+      let res = {}
+
+      middleware(req, res, next)
+
+      expect(req.context.correlationId).to.not.be.an('undefined')
+      expect(req.context.correlationId).to.not.be.null
+    })
+  })
+
+  context('when client supply a correlation id in request', () => {
+    it('should use correlation id from querystring', () => {
+      const expected = 'AbC123'
+      let req = {
+        url: `/api/path?foo=bar&correlationId=${expected}`
+      }
+      let res = {}
+
+      middleware(req, res, next)
+
+      expect(req.context.correlationId).to.equal(expected)
+    })
   })
 })
