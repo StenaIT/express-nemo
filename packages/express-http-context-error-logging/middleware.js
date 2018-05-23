1202eaabed0ced5b8d9b737714f59b4e13649f91
Giorgio Salmi <giorgio.salmi@stena.com>
2018-05-23 17:51:57 +0200

    - Add error-logging

---
+++ packages/express-http-context-error-logging/middleware.js
@@ -0,0 +1,28 @@
+const extend = require('deep-extend')
+
+const defaults = {
+  createLogger: () => console,
+  eventTemplate: (err, req) => `Unandled error: ${err.name}, ${err.message}`
+}
+
+module.exports = (opt) => {
+  const options = extend({}, defaults, opt)
+
+  const getLogger = (req) => {
+    if (req.context && req.context.logger) {
+      return req.context.logger
+    }
+    return null
+  }
+
+  const middleware = (err, req, res, next) => {
+    const logger = getLogger(req) || options.createLogger()
+
+    logger.error(options.eventTemplate(err, req))
+
+    next()
+  }
+
+  middleware.options = options
+  return middleware
+}
---
+++ packages/express-http-context-error-logging/middleware.spec.js
@@ -0,0 +1,73 @@
+const expect = require('chai').expect
+const middleware = require('./middleware')
+
+describe('express-http-context-error-logging', () => {
+  let nextCalled = false
+  let err = new Error('Test error')
+  let req = { url: '/api/path' }
+  let res = {}
+  let logs = []
+
+  const next = () => {
+    nextCalled = true
+  }
+
+  beforeEach(() => {
+    nextCalled = false
+    logs = []
+  })
+
+  const testOptions = {
+    createLogger: () => {
+      return {
+        error: message => logs.push(message)
+      }
+    },
+    eventTemplate: (err, req) => 'test'
+  }
+
+  context('should be a configurable middleware', () => {
+    it('should store middleware options for us to inspect', () => {
+      let mw = middleware({})
+      expect(mw.options).to.not.be.undefined
+    })
+    context('defaults', () => {
+      it('default logger (console) if no one is specified', () => {
+        let mw = middleware({})
+        expect(mw.options.createLogger).to.be.a('function')
+      })
+
+      it('default eventTemplate if no one is specified', () => {
+        let mw = middleware({})
+        expect(mw.options.eventTemplate).to.be.a('function')
+      })
+    })
+
+    context('overrides', () => {
+      it('override logger with our own', () => {
+        let mw = middleware(testOptions)
+        expect(mw.options.createLogger).to.be.equal(testOptions.createLogger)
+      })
+
+      it('override eventTemplate with our own', () => {
+        let mw = middleware(testOptions)
+        expect(mw.options.eventTemplate).to.be.equal(testOptions.eventTemplate)
+      })
+    })
+  })
+
+  it('should always call next', () => {
+    let mw = middleware(testOptions)
+    mw(err, req, res, next)
+
+    expect(nextCalled).to.be.true
+  })
+
+  it('should log an error event', () => {
+    let mw = middleware(testOptions)
+    mw(err, req, res, next)
+
+    expect(logs.length).to.be.equal(1)
+    expect(logs[0]).to.be.equal('test')
+  })
+})
